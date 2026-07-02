e4c9c0d1aab0d27bee52b6b4cd9d9a7cb41bbac9
Cloud <f9xyz7@gmail.com>
2026-07-02 14:44:25 +0300

    Create __init__.py

---
+++ backend/database/models/__init__.py
@@ -0,0 +1,4 @@
+# backend/database/models/__init__.py
+from backend.database.models.user import User
+from backend.database.models.threat import Threat
+from backend.database.models.alert import Alert
